f70fca3a7656873ecaccef9e6cdac27b30bc7a9c
albertee <albert.li@cardinfolink.com>
2017-10-30 14:23:45 +0800

    XHR & Dependency Injection

--- app/components/phone-list/phone-list.component.js
+++ app/components/phone-list/phone-list.component.js
@@ -1,24 +1,19 @@
-var PhoneListController = function () {
-    this.phones = [
-        {
-            name: 'Nexus S',
-            age: 1,
-            snippet: 'Fast just got faster with Nexus S.'
-        }, {
-            name: 'Motorola XOOM™ with Wi-Fi',
-            age: 2,
-            snippet: 'The Next, Next Generation tablet.'
-        }, {
-            name: 'MOTOROLA XOOM™',
-            age: 3,
-            snippet: 'The Next, Next Generation tablet.'
-        }
-    ];
+var PhoneListController = function ($http) {
     this.orderProp = 'age';
+    $http.get('../../phones/phones.json').then(res => {
+        this.phones = res.data;
+    })
 };
 
 // register component "phoneList" ==> module "phoneList"
 angular.module('phoneList').component('phoneList', {
     templateUrl: 'components/phone-list/phone-list.template.html',
-    controller: PhoneListController
+    controller: ['$http', PhoneListController]
 });
+
+// 依赖注入 ==> 参数名实现
+// 禁止压缩依赖名称列表
+// 方法一：$inject
+// xxxController.$inject = ['$http']
+// 方法二：内联注解数组
+// controller: ['$http',xxxController]
