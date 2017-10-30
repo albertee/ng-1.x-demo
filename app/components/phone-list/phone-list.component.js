12d2f993c92a9be6abf17ff4598246490a26968b
albertee <albert.li@cardinfolink.com>
2017-10-30 14:05:33 +0800

    two way data binding ==> orderProp

--- app/components/phone-list/phone-list.component.js
+++ app/components/phone-list/phone-list.component.js
@@ -2,15 +2,19 @@ var PhoneListController = function () {
     this.phones = [
         {
             name: 'Nexus S',
+            age: 1,
             snippet: 'Fast just got faster with Nexus S.'
         }, {
             name: 'Motorola XOOM™ with Wi-Fi',
+            age: 2,
             snippet: 'The Next, Next Generation tablet.'
         }, {
             name: 'MOTOROLA XOOM™',
+            age: 3,
             snippet: 'The Next, Next Generation tablet.'
         }
-    ]
+    ];
+    this.orderProp = 'age';
 };
 
 // register component "phoneList" ==> module "phoneList"
